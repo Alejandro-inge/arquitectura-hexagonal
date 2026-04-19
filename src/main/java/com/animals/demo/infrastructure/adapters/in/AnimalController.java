4f00fda81f32c853a56df8e7231a53c8d4dfc70e
Alejandro-inge <galvanalejandro@protonmail.com>
2026-04-19 02:56:31 -0600

    Update AnimalController.java
    Fix package

--- src/main/java/com/animals/demo/infrastructure/adapters/in/AnimalController.java
+++ src/main/java/com/animals/demo/infrastructure/adapters/in/AnimalController.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package com.animals.demo.infrastructure.adapters.out;
+package com.animals.demo.infrastructure.adapters.in;
 
 import java.util.List;
 
